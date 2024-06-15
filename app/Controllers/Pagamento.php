c45b8be8c69463c53a804c7ed2b45288492db778
Juan Cardoso <juancardoso@outlook.com>
2024-06-15 10:01:41 -0300

    Separar o header

--- app/Controllers/Pagamento.php
+++ app/Controllers/Pagamento.php
@@ -2,11 +2,13 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Controller;
+
 use chillerlan\QRCode\{QRCode, QROptions};
 
 class Pagamento extends BaseController
 {
-    public function index(): string
+    public function index()
     {
         $url = 'https://facens.br/';
         $qrcode = (new QRCode)->render($url);
--- app/Views/HomeView.php
+++ app/Views/HomeView.php
@@ -1,56 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Page Title</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel="stylesheet" href="<?php echo base_url() ?>public/css/styles.css"">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url() ?>public/js/home.js"></script>
-    <script>
-      function redirectToHome(){
-        window.location.href="<?php echo base_url()?>Home"
-      }  
-
-      function redirectToPayment(){
-        window.location.href="<?php echo base_url()?>Pagamento"
-      }  
-    </script>
-</head>
-<body style="margin: 0;">
-<div class="header">
-  <img class="center-block" style="width: 337px;" src="<?php echo base_url() ?>public/images/FacensPlay.JPG" onclick="redirectToHome()" />
-  <input class="search-input center-block" style="cursor: pointer; background: url('<?php echo base_url() ?>public/images/search.svg') no-repeat right"/>
-  <img class="center-block" width="30px" src="<?php echo base_url() ?>public/images/money.svg" onclick="redirectToPayment()">
-  <img class="center-block" width="30px" src="<?php echo base_url() ?>public/images/profile.svg">
-  </div>
-</div>
-  
-<div class="menu">
-  <div class="menu-item">
-    Desenvolvimento
-  </div>
-  <div class="menu-item">
-    Finanças
-  </div>
-  <div class="menu-item">
-    Design
-  </div>
-  <div class="menu-item">
-    Marketing
-  </div>
-  <div class="menu-item">
-    Produtividade
-  </div>
-  <div class="menu-item">
-    Negocios
-  </div>
-</div>
+<?php include 'header.php' ?>
 <div class="content">
   <div id="header"></div>
   <div class="container mt-5"><!--style="width: 1661px; height: 607px; left: 33px; top: 284px; position: absolute"-->
